b1bc85635f139893ad0f520182cdc2927cf4463f
Oswald Calvario <oswald.calvario@gmail.com>
2019-05-04 23:41:08 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { Wagner: "Die Walkyrie", Mozart: "Magic Flute"}
+
+function updatePlaylist (playlist, artistName, songTitle) {
+  playlist[artistName] = 'songTitle';
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artistName) {
+  delete playlist.artistName;
+ return playlist;
+}
